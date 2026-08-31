784f7a55924bb3e832259d00e6805d16e3d5a576
welchellen <welchellen@icloud.com>
2026-08-30 21:39:08 -0500

    git add . git commit -m "Phase 2 Complete" git push origin main

--- journal.md
+++ journal.md
@@ -1,3 +1,8 @@
 Phase 1: ## Phase 1
 
 If I tried to change the character's name later in the program, JavaScript would give me an error because I created the character name using `const`. A constant cannot be reassigned after it has been created. I used `let` for health and experience points because those values need to be able to change during the game.
+
+## Phase 2
+
+The compound assignment operator made my code shorter and easier to read. Instead of writing `currentHealth = currentHealth + 20`, I could write `currentHealth += 20`. Both do the same thing, but `+=` makes it easier to update values that are already stored in variables.
+
--- script.js
+++ script.js
@@ -10,3 +10,28 @@ console.log("Home World: " + homeWorld);
 console.log("Class: " + baseClass);
 console.log("Health: " + currentHealth);
 console.log("XP: " + experiencePoints);
+const charactername = "Luna";
+const homeworld= "Eldoria";
+const baseclass = "Mage";
+
+let currenthealth = 100;
+let experiencepoints = 0;
+
+console.log("Character Name: " + characterName);
+console.log("Home World: " + homeWorld);
+console.log("Class: " + baseClass);
+console.log("Health: " + currentHealth);
+console.log("XP: " + experiencePoints);
+
+let powerLevel = (currentHealth * 0.5) + (experiencePoints * 10);
+
+console.log("Starting Power Level: " + powerLevel);
+
+currentHealth += 20;
+experiencePoints += 50;
+
+powerLevel = (currentHealth * 0.5) + (experiencePoints * 10);
+
+console.log("Health after potion: " + currentHealth);
+console.log("XP after potion: " + experiencePoints);
+console.log("Updated Power Level: " + powerLevel);
