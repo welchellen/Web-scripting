d266e43f23dd376180f7545f6f3cac0adfcc5ab4
welchellen <welchellen@icloud.com>
2026-08-30 21:34:32 -0500

    commit

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-console.log("Script Loaded")
-let username = "Any";
-alert ("Welcome to the Fitness Tracker, "+ "username" + "!" ) 
-let weighlifted= Number(prompt("How much weight did you lift?"))
-let repsPerformed= Number(prompt("How many reps did you perform"))
-let totalvolume= weighlifted* repsPerformed;
-console.log(totalvolume) 
-document.getElementById("status").textContent = statusmessage
+const characterName = "Luna";
+const homeWorld = "Eldoria";
+const baseClass = "Mage";
 
-let statusmessage = ("Great Job! \n You moved 500 units today.") 
+let currentHealth = 100;
+let experiencePoints = 0;
+
+console.log("Character Name: " + characterName);
+console.log("Home World: " + homeWorld);
+console.log("Class: " + baseClass);
+console.log("Health: " + currentHealth);
+console.log("XP: " + experiencePoints);
